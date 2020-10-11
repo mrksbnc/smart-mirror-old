6a251e72ac488ecac739b484bf0a0b7de85f1e0b
Bence Márkus <markusbence@outlook.com>
2020-10-11 13:34:25 +0200

    console.log() removed from files

--- src/renderer/components/Email/Email.vue
+++ src/renderer/components/Email/Email.vue
@@ -33,10 +33,6 @@ export default {
     CheckCircle
   },
 
-  mounted () {
-    console.log(this.$data)
-  },
-
   computed: {
     emails: function () {
       return this.$store.state.email.email
